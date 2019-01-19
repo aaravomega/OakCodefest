bdf306edeb9b42e52c76f461c1c136e189410a95
Aarav Agarwal <aaravagarwal.amaze@gmail.com>
2019-01-19 17:44:24 +0530

    First commit

---
+++ raspberry_pi_car.py
@@ -0,0 +1,58 @@
+#made on raspberry pi
+#we have represented it in the form of a WORKING CAR (4WD) ROBOT
+from gpiozero import MotionSensor
+import RPi.GPIO as gpio
+import time
+import sys
+import pygame
+from pygame.locals import *
+
+pir = MotionSensor(4)
+
+
+def init():
+   # gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+def reverse(tf):
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    
+def forward(tf):
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    
+def turn_right(tf):
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    
+def turn_left(tf):
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+
+def stop(tf):
+    gpio.output(7, False)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, False)
+    time.sleep(tf)
+    #gpio.cleanup()
+
+    forward(3)
+    stop(1)
+    
